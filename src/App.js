797860a2bc348ba43c3e5647713fe924833fe46d
Gqq475 <542764124@qq.com>
2016-08-02 16:51:47 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React,{PropTypes} from 'react';
+import axios from 'axios';
+import CircularProgress from 'material-ui/CircularProgress';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 
 class App extends React.Component {
+  getChildContext() {
+    return {muiTheme: getMuiTheme()};
+  }
+  constructor(){
+    super();
+    this.state={
+      info:{},
+      wait:true
+    }
+  }
+  componentDidMount(){
+    axios.get('https://api.github.com/users/Gqq475')
+    .then(function(res){
+      this.setState({
+        info:res.data,
+        wait:false
+      })
+    }.bind(this))
+  }
   render () {
+    let x =  <CircularProgress />
     return(
-      <h1>脚手架工具</h1>
+      <div>
+        {this.state.wait ? x:
+           <div>
+             {this.state.info.login}
+             <img src={this.state.info.avatar_url} />
+           </div>
+       }
+      </div>
     )
   }
 }
-
+App.childContextTypes = {
+  muiTheme: React.  PropTypes.object.isRequired,
+};
 export default App;
